e36d3c8d7338f3831e1e124594719c7f2ec7f53e
kzholik <kris986@mail.ru>
2020-01-31 15:04:52 +0100

    [VTO] Integration with Sentry SDK

--- external/veikkaus_base.py
+++ external/veikkaus_base.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-from time import sleep
-
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
@@ -16,16 +13,13 @@ class VeikkausBase:
         game_dict = dict()
         box = self.collect_data_on_page(driver)
         if box:
-            print(len(box))
             for element in box:
-                print(element.text)
                 soup = BeautifulSoup(element.get_attribute('innerHTML'), 'html.parser')
                 details = soup.find('div', {'class': 'event-row'})
                 odd_dict = dict()
                 for detail in details:
                     source_list = list()
                     dtls_dict = dict()
-                    # odd_dict = dict()
                     checker_class = detail.get_attribute_list('class')
                     if 'event-info' in checker_class:
                         event_title = detail.find('span', {'class': 'event-title'}).text
@@ -50,17 +44,13 @@ class VeikkausBase:
                         dtls_dict[source_name] = odd_dict
                         game_dict[event_title] = source_list
                         source_list.append(dtls_dict)
-            # print(game_dict)
         return game_dict
 
     def collect_data_on_page(self, driver):
         try:
-            # self.odds_base.wait_visibility_css_selector(driver, 'div#pitkaveto-sub-navigation-container', timeout=40)
-            print('div#pitkaveto-sub-navigation-container EXISTS')
+            self.odds_base.wait_visibility_css_selector(driver, 'div#pitkaveto-sub-navigation-container', timeout=40)
             box = driver.find_elements_by_xpath(
                 "//li[(contains(@class,'event-group-event')  and not(contains(@class, 'target-row')) and not(contains(@class, 'hidden')))]")
-            print('Boxes with data were founded')
-            print(type(box))
             return box
         except(NoSuchElementException, TimeoutException) as e:
             capture_exception(e)
